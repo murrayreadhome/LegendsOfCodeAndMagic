fb04b4ea587833e14e3d4ab1c13506a3974e9534
Murray <murray@djac>
2019-12-18 13:28:04 +0000

    initial commit

---
+++ LegendsOfCodeAndMagic.cpp
@@ -0,0 +1,12 @@
+﻿// LegendsOfCodeAndMagic.cpp : Defines the entry point for the application.
+//
+
+#include "LegendsOfCodeAndMagic.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
